5cb3f3d3c5d4f5d252d3383647e02a7e8f1119e7
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-07-10 17:11:15 -0700

    use GitHub API to fetch repo info

--- intern.js
+++ intern.js
@@ -5,8 +5,19 @@
 //  Copyright 2014 Chorded Constructions. All rights reserved.
 //
 
+var fs = require('fs');
+var path = require('path');
+
 var request = require('request');
 var cheerio = require('cheerio');
+var github = require('octonode');
+
+var client = github.client({
+  id: process.env.GITHUB_ID,
+  secret: process.env.GITHUB_SECRET
+});
+
+// flow: find repos (SCRAPE) -=> get repo info (API) -=> save to S3 as blocks.json
 
 var repos = [];
 
@@ -26,7 +37,9 @@ function findReposOnPage(url, callback) {
     var $ = cheerio.load(body);
     $('div.code-list-item').each(function () {
       var href = $(this).find('p.title a').attr('href');
-      repos.push({href: href});
+      // trim to form 'OWNER-LOGIN/REPO-NAME'
+      var fullname = /\/?([\w-]+\/[\w-]+)$/.exec(href)[1];
+      repos.push(fullname);
     });
 
     var nextPage = $('div.pagination a.next_page').attr('href');
@@ -52,4 +65,49 @@ function findRepos(fragment) {
 
 // NB - global search not yet available via API, scrape in the interim. 😁
 //  https://developer.github.com/changes/2013-10-18-new-code-search-requirements/
-findRepos('/search?q=cinder+path%3A%2Fcinderblock.xml&type=Code');
+// findRepos('/search?q=cinder+path%3A%2Fcinderblock.xml&type=Code');
+
+// skip scraper during development and use saved results
+repos = JSON.parse(fs.readFileSync('./repos.json'));
+
+var block = {
+  id: '',
+  name: '',
+  owner: {
+    id: '',
+    name: '',
+    url: '',
+    avatar_url: ''
+  },
+  description: '',
+  url: '',
+  created: null,
+  updated: null,
+  star_count: 0,
+//  image: null,
+//  sample_count: 0,
+//  forks: []
+};
+
+var randomIndex = Math.floor(Math.random() * (repos.length + 1));
+var repo = client.repo(repos[randomIndex]);
+repo.info(function (err, data, headers) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  block.id = data.id;
+  block.name = data.name;
+  block.owner.id = data.owner.id;
+  block.owner.name = data.owner.login;
+  block.owner.url = data.owner.html_url;
+  block.owner.avatar_url = data.owner.avatar_url;  
+  block.description = data.description;
+  block.url = data.html_url;
+  block.created = data.created_at;
+  block.updated = data.updated_at;
+  block.star_count = data.stargazers_count;
+
+  console.log(block);
+});
