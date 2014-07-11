a016b160a67e3b46edc245f33b1ebb9bfbad58fa
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2014-07-10 20:19:03 -0700

    better flow page scraping and use async

--- intern.js
+++ intern.js
@@ -7,7 +7,9 @@
 
 var fs = require('fs');
 var path = require('path');
+var util = require('util');
 
+var async = require('async');
 var request = require('request');
 var cheerio = require('cheerio');
 var github = require('octonode');
@@ -17,23 +19,23 @@ var client = github.client({
   secret: process.env.GITHUB_SECRET
 });
 
-// flow: find repos (SCRAPE) -=> get repo info (API) -=> save to S3 as blocks.json
+// flow: find block repos (SCRAPE) -=> get repo info (API) -=> save to S3 as blocks.json
 
-var repos = [];
-
-function findReposOnPage(url, callback) {
+// NB - global search not yet available via API, scrape in the interim. 😁
+//  https://developer.github.com/changes/2013-10-18-new-code-search-requirements/
+function findBlocksOnPage(page, callback) {
+  var url = util.format("https://github.com/search?p=%s&q=cinder+path%3A%2Fcinderblock.xml&type=Code", page);
   request.get(url, {}, function (err, res, body) {
     if (err) {
-      console.error('ERROR - ' + err);
       callback(err);
       return;
     }
     if (res.statusCode != 200) {
-      console.error('ERROR - status ' + res.statusCode);
-      callback(res.statusCode);
+      callback({statusCode: res.statusCode});
       return;
     }
 
+    var repos = [];
     var $ = cheerio.load(body);
     $('div.code-list-item').each(function () {
       var href = $(this).find('p.title a').attr('href');
@@ -42,72 +44,93 @@ function findReposOnPage(url, callback) {
       repos.push(fullname);
     });
 
-    var nextPage = $('div.pagination a.next_page').attr('href');
-    callback(null, nextPage);
-  });
+    callback(null, repos);
+  });  
 }
 
-function findRepos(fragment) {
-  var url = 'https://github.com' + fragment;
-  findReposOnPage(url, function (err, nextPage) {
+function getBlock(full_name, callback) {
+  client.repo(full_name).info(function (err, data, headers) {
     if (err) {
+      callback(err);
       return;
     }
 
-    if (nextPage) {
-      findRepos(nextPage);
-    } else {
-      console.log('repos ' + repos.length);
-      console.log(repos);
-    }
+    var block = {
+      id: data.id,
+      name: data.name,
+      full_name: data.full_name,
+      owner: {
+        id: data.owner.id,
+        name: data.owner.login,
+        url: data.owner.html_url,
+        avatar_url: data.owner.avatar_url
+      },
+      description: data.description,
+      url: data.html_url,
+      created: data.created_at,
+      updated: data.updated_at,
+      star_count: data.stargazers_count,
+//      image: null,
+//      sample_count: 0,
+//      forks: []
+    };
+    callback(null, block);
+  });  
+}
+
+function hasImage(name, callback) {
+  // TODO - use data.default_branch from client.repo(fullname).info()
+  var url = util.format("https://raw.githubusercontent.com/%s/master/cinderblock.png", name);
+  request.head(url, {}, function (err, res, body) {
+    callback(err, res.statusCode === 200);
   });
 }
 
-// NB - global search not yet available via API, scrape in the interim. 😁
-//  https://developer.github.com/changes/2013-10-18-new-code-search-requirements/
-// findRepos('/search?q=cinder+path%3A%2Fcinderblock.xml&type=Code');
+var main = function () {
+  var repos = [];
+  var status = true;
+  var page = 1;
+  async.whilst(function () {
+    return status;
+  }, function (callback) {
+    findBlocksOnPage(page++, function (err, data) {
+      if (err) {
+        callback(err);
+        return;
+      }
 
-// skip scraper during development and use saved results
-repos = JSON.parse(fs.readFileSync('./repos.json'));
+      repos = repos.concat(data);
+      status = data.length > 0;
+      callback();
+    });
+  }, function (err) {
+    if (err) {
+      console.error('ERROR - ', err);
+      return;
+    }
 
-var block = {
-  id: '',
-  name: '',
-  owner: {
-    id: '',
-    name: '',
-    url: '',
-    avatar_url: ''
-  },
-  description: '',
-  url: '',
-  created: null,
-  updated: null,
-  star_count: 0,
-//  image: null,
-//  sample_count: 0,
-//  forks: []
+    console.log("found %d block repos", repos.length);
+  });
+  return;
+
+/*
+  // skip scraper during development and use saved results
+  var repos = JSON.parse(fs.readFileSync('./repos.json'));
+  repos = repos.slice(0, 3);
+
+  var blocks = [];
+  async.mapLimit(repos, 1, getBlock, function (err, results) {
+    if (err) {
+      console.error('ERROR - ', err);
+      return;
+    }
+  
+    blocks = results;
+    console.log(blocks);
+  });
+*/
 };
 
-var randomIndex = Math.floor(Math.random() * (repos.length + 1));
-var repo = client.repo(repos[randomIndex]);
-repo.info(function (err, data, headers) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  block.id = data.id;
-  block.name = data.name;
-  block.owner.id = data.owner.id;
-  block.owner.name = data.owner.login;
-  block.owner.url = data.owner.html_url;
-  block.owner.avatar_url = data.owner.avatar_url;  
-  block.description = data.description;
-  block.url = data.html_url;
-  block.created = data.created_at;
-  block.updated = data.updated_at;
-  block.star_count = data.stargazers_count;
-
-  console.log(block);
-});
+if (require.main === module) {
+  main();
+}
