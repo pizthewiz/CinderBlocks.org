60f04151f4b497de3817217ea9041b18f66d6527
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2015-01-09 11:35:20 -0800

    add basic build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,72 @@
 
 var gulp = require('gulp');
 
+// site
 var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
+var clean = require('gulp-clean');
 
-var intern = require('./intern.js');
-
+gulp.task('lint', function() {
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'))
+  .pipe(jshint.reporter('fail'));
+});
+gulp.task('clean', function() {
+  gulp.src('./dist/*')
+  .pipe(clean({force: true}));
+});
+gulp.task('minify-css', function() {
+  var opts = {comments: true, spare: true};
+  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+  .pipe(minifyCSS(opts))
+  .pipe(gulp.dest('./dist/'))
+});
+gulp.task('minify-js', function() {
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+  .pipe(uglify({
+    // inSourceMap:
+    // outSourceMap: "app.js.map"
+  }))
+  .pipe(gulp.dest('./dist/'))
+});
+gulp.task('copy-bower-components', function () {
+  gulp.src('./app/bower_components/**')
+  .pipe(gulp.dest('dist/bower_components'));
+});
+gulp.task('copy-html-files', function () {
+  gulp.src('./app/**/*.html')
+  .pipe(gulp.dest('dist/'));
+});
+gulp.task('copy-images', function () {
+  gulp.src('./app/**/*.{png,svg,ico}')
+  .pipe(gulp.dest('dist/'));
+});
 gulp.task('connect', function () {
   connect.server({
     root: 'app/',
     port: 8888
   });
 });
+gulp.task('connectDist', function () {
+  connect.server({
+    root: 'dist/',
+    port: 9999
+  });
+});
+
+gulp.task('default',
+  ['lint', 'connect']
+);
+gulp.task('build',
+  ['lint', 'minify-css', 'minify-js', 'copy-bower-components', 'copy-html-files', 'copy-images', 'connectDist']
+);
 
 // intern
+var intern = require('./intern.js');
+
 gulp.task('find:users', function (cb) {
   intern.findUsers(function (err, data) {
     if (err) {
