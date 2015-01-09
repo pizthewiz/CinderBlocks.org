fb02bdbc31fc9d7f51e9d14469694abc721adf0f
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2015-01-09 12:37:14 -0800

    improve readablilty a bit

--- gulpfile.js
+++ gulpfile.js
@@ -11,44 +11,44 @@ var minifyCSS = require('gulp-minify-css');
 var clean = require('gulp-clean');
 
 gulp.task('lint', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'))
-  .pipe(jshint.reporter('fail'));
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**']).
+    pipe(jshint()).
+    pipe(jshint.reporter('default')).
+    pipe(jshint.reporter('fail'));
 });
 gulp.task('clean', function() {
-  gulp.src('./dist/*')
-  .pipe(clean({force: true}));
+  gulp.src('./dist/*').
+    pipe(clean({force: true}));
 });
 gulp.task('minify-css', function() {
   var opts = {comments: true, spare: true};
-  gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
-  .pipe(minifyCSS(opts))
-  .pipe(gulp.dest('./dist/'))
+  gulp.src(['./app/**/*.css', '!./app/bower_components/**']).
+    pipe(minifyCSS(opts)).
+    pipe(gulp.dest('./dist/'));
 });
 gulp.task('minify-js', function() {
-  gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
-  .pipe(uglify({
-    // inSourceMap:
-    // outSourceMap: "app.js.map"
-  }))
-  .pipe(gulp.dest('./dist/'))
+  gulp.src(['./app/**/*.js', '!./app/bower_components/**']).
+    pipe(uglify({
+      // inSourceMap:
+      // outSourceMap: "app.js.map"
+    })).
+    pipe(gulp.dest('./dist/'));
 });
 gulp.task('copy-bower-components', function () {
-  gulp.src('./app/bower_components/**')
-  .pipe(gulp.dest('dist/bower_components'));
+  gulp.src('./app/bower_components/**').
+    pipe(gulp.dest('dist/bower_components'));
 });
 gulp.task('copy-html-files', function () {
-  gulp.src('./app/**/*.html')
-  .pipe(gulp.dest('dist/'));
+  gulp.src('./app/**/*.html').
+    pipe(gulp.dest('dist/'));
 });
 gulp.task('copy-image-files', function () {
-  gulp.src('./app/**/*.{png}')
-  .pipe(gulp.dest('dist/'));
+  gulp.src('./app/**/*.{png,svg}').
+    pipe(gulp.dest('dist/'));
 });
 gulp.task('copy-text-files', function () {
-  gulp.src('./app/**/*.txt')
-  .pipe(gulp.dest('dist/'));
+  gulp.src('./app/**/*.txt').
+    pipe(gulp.dest('dist/'));
 });
 gulp.task('connect', function () {
   connect.server({
